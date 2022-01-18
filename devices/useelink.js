1bd9b53c57a439174b05a66e0213c0cb1ac831c3
Ben Turnbull <benjamin.turnbull@gmail.com>
2022-01-19 09:15:23 +1100

    Update useelink.js
    adds support for useelink SM-SO301AZ - which is functionally similar but identifies differently to another useelink product.

--- devices/useelink.js
+++ devices/useelink.js
@@ -23,6 +23,27 @@ module.exports = [
         },
     },
     {
+        fingerprint: [{modelID: 'TS011F', manufacturerName: '_TZ3000_cfnprab5'}],
+        model: 'SM-SO301AZ',
+        vendor: 'UseeLink',
+        description: '4 gang switch, with USB',
+        extend: extend.switch(),
+        exposes: [e.switch().withEndpoint('l1'), e.switch().withEndpoint('l2'),
+            e.switch().withEndpoint('l3'), e.switch().withEndpoint('l4'), e.switch().withEndpoint('l5')],
+        meta: {multiEndpoint: true},
+        endpoint: (device) => {
+            return {l1: 1, l2: 2, l3: 3, l4: 4, l5: 5};
+        },
+        configure: async (device, coordinatorEndpoint, logger) => {
+            await reporting.bind(device.getEndpoint(1), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(2), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(3), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(4), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(5), coordinatorEndpoint, ['genOnOff']);
+        },
+    },
+
+    {
         fingerprint: [{modelID: 'TS011F', manufacturerName: '_TZ3000_tvuarksa'}],
         model: 'SM-AZ713',
         vendor: 'UseeLink',
